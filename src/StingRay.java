94b6560c7b32323df6bf1144f3d83ccd29c32dc3
FractalFox <flgtom@aol.com>
2017-08-02 13:07:26 -0700

    StingRay() class added

---
+++ src/StingRay.java
@@ -0,0 +1,11 @@
+public class StingRay extends MyCar{
+
+    public StingRay() {
+        System.out.println("Null StingRay Created");
+    }
+
+    public StingRay(String name, String wheels, String direction, int speed, int gear, String model, String doors, String engine) {
+        super(name, wheels, direction, speed, gear, model, doors, engine);
+
+    }
+}
